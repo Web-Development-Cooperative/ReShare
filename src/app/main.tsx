03927a0b5f12b6b0327de654a4f6fadd19502e54
Emmanuil <emmanuil.litvinenko@inbox.ru>
2026-03-19 13:06:22 +0500

    feat: apply modified styles

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
+import '~~>shared/assets/styles/reset.css';
+import '~~>shared/assets/styles/index.css';
 
 createRoot(document.getElementById('root')!).render(<App />);
