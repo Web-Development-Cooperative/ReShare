9c49a66b7d0ae7657f7c33e3e3545a9b0ce8dcfe
Emmanuil <emmanuil.litvinenko@inbox.ru>
2026-03-20 17:59:12 +0500

    feat: add one more paragraph

--- src/shared/ui/paragraphs/index.ts
+++ src/shared/ui/paragraphs/index.ts
@@ -3,3 +3,4 @@ export { UIText14Reg } from './uiText14Reg/UIText14Reg';
 export { UIText14Medium } from './uiText14Medium/UIText14Medium';
 export { Paragraph16Reg } from './paragraph16Reg/Paragraph16Reg';
 export { Heading36 } from './heading36/Heading36';
+export { Heading24 } from './heading24/Heading24';
