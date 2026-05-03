b01e83da4206869995fe9bef4e60f64eed9905a0
Emmanuil <emmanuil.litvinenko@inbox.ru>
2026-05-03 15:59:51 +0500

    feat: add field validation and change the structure of the form

--- src/features/login/ui/LoginForm.tsx
+++ src/features/login/ui/LoginForm.tsx
@@ -2,16 +2,22 @@ import { AuthForm } from '@shared/ui/others';
 
 import { useLoginForm } from '../lib/useLoginForm';
 
+import type {
+	LoginUserCommand,
+	RegisterUserCommand,
+} from '@shared/api/generated/identity-api';
+
 const LoginForm = () => {
-	const { data, onChange, onSubmit, isLoading } = useLoginForm();
+	const { data, onChange, onSubmit, isLoading, errors } = useLoginForm();
 
 	return (
 		<AuthForm
 			typeForm="login"
-			value={data}
+			value={data as unknown as RegisterUserCommand & LoginUserCommand}
 			onChange={onChange}
 			onSubmit={onSubmit}
 			isLoading={isLoading}
+			errors={errors}
 		/>
 	);
 };
